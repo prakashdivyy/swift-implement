9200f33dfdb2e61ba212e276c927caa83104b22c
Desi Ratna Mukti <desi.rmu@gmail.com>
2016-12-27 10:45:39 +0700

    benerin navbar

--- templates/home.php
+++ templates/home.php
@@ -15,13 +15,13 @@
 <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Kelompok 2 Sisdis</a>
         <ul class="right hide-on-med-and-down">
-            <li><a href="grup2-ceph-04.sisdis.ui.ac.id/imp-swift/">Home</a></li>
-            <li><a href="grup2-ceph-04.sisdis.ui.ac.id/imp-swift/gallery">Gallery</a></li>
+            <li><a href="/imp-swift/">Home</a></li>
+            <li><a href="/imp-swift/gallery">Gallery</a></li>
             <li><a href="grup2-ceph-04.sisdis.ui.ac.id/imp-s3/">S3</a></li>
         </ul>
         <ul id="nav-mobile" class="side-nav">
-            <li><a href="grup2-ceph-04.sisdis.ui.ac.id/imp-swift/">Home</a></li>
-            <li><a href="grup2-ceph-04.sisdis.ui.ac.id/imp-swift/gallery">Gallery</a></li>
+            <li><a href="/imp-swift/">Home</a></li>
+            <li><a href="/imp-swift/gallery">Gallery</a></li>
             <li><a href="grup2-ceph-04.sisdis.ui.ac.id/imp-s3/">S3</a></li>
         </ul>
         <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
